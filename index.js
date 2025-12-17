2b70cc92487388fdcb1dca4776a650a6ea34ee9c
chris-mmv <chris.mmv@gmail.com>
2025-12-17 17:34:36 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code here
+const http = require('http');
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+//log message to console
+    console.log('Request received' + req.method + ' request for: ' + req.url);
+    //send headers
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //send body response
+    res.end('Hello World');
+});
+
+server.listen(port, 'localhost', null, () => {
+    //log message to console
+    console.log(`Server running at http://localhost:${port}/`);
+});
